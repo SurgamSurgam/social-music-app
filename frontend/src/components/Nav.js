cc644f4a7db9854974192579a84ac512575ee89d
Deyvi <deyvio@gmail.com>
2019-03-18 12:10:27 -0400

    Used NavLink instead to use active

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Nav = () => {
   return (
     <nav>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/songs"}>All Songs</Link>
-      <Link to={"/songs/bypop"}>By Popularity</Link>
-      <Link to={"/songs/bygenre"}>By Genre</Link>
-      <Link to={"/profile"}>My Profile</Link>
+      <NavLink to={"/"}>Home</NavLink>
+      <NavLink to={"/songs"}>All Songs</NavLink>
+      <NavLink to={"/songs/bypop"}>By Popularity</NavLink>
+      <NavLink to={"/songs/bygenre"}>By Genre</NavLink>
+      <NavLink to={"/profile"}>My Profile</NavLink>
     </nav>
   );
 };
